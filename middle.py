ec5a3114faedb784cfdb14c6aaf586616738332a
Samuel Zhang <samuelzhang3@gmail.com>
2017-09-14 17:26:59 -0400

    init

--- middle.py
+++ middle.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
+if __name__ == "__main__":
+    app.debug = True # change to false when deploying
+    app.secret_key = str(uuid4())
+    app.run()
