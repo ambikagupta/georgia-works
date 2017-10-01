1980a2929e3a899fe13ae86890d7febbccc568b7
Austin Cho <AustinCho42@gmail.com>
2017-10-01 14:46:27 -0400

    Removed login folder

--- middle.py
+++ middle.py
@@ -1,10 +1,37 @@
 from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Hello World!"
+    if not session.get('logged_in'):
+        return render_template('login2.html')
+    else:
+        return render_template('loggedIn.html')
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return home()
+
+@app.route("/forgot")
+def forgotPassword():
+    return render_template('forgot.html')
+
+@app.route("/forgot", methods=['POST'])
+def passwordReset():
+    return "Hello"
 
 if __name__ == "__main__":
-    app.debug = True # change to false when deploying
+    app.secret_key = os.urandom(12)
     app.run()
